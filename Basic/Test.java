1d9ab6740a0726b5bd2d6730c23961eb8dbdee51
Sibindra <timalsinasibindra@gmail.com>
2021-11-16 12:31:50 +0545

    ...

---
+++ Basic/Test.java
@@ -0,0 +1,40 @@
+class Student {
+    
+    String name;
+    int roll;
+    static String collegeName="NCIT";
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setRoll(int roll){
+        this.roll=roll;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getRoll(){
+        return this.roll;
+    }
+
+    static public String getcollegeName(){
+        return collegeName;
+    }
+
+}
+
+class Test {
+    public static void main(String[] arg){
+
+        Student obj=new Student();
+
+        obj.setName("Hari");
+        obj.setRoll(22);
+
+        System.out.println(obj.getName());
+        Student.getcollegeName();
+    }
+}
