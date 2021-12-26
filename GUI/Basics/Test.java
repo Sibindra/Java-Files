b6bcf1cf270cf448ba11a09767061b7018145c64
Sibindra <timalsinasibindra@gmail.com>
2021-12-26 20:07:53 +0545

    mouse events completed and added some comments to basics

--- GUI/Basics/Test.java
+++ GUI/Basics/Test.java
@@ -4,17 +4,19 @@ class GUI{
 
     public GUI(){
         JFrame f=new JFrame();
-        f.setSize(400,500);
+        f.setSize(400,500); //(w,h)
 
         JTextField t1=new JTextField();
-        t1.setBounds(100,100,200,20);
+        t1.setBounds(100,100,200,20); //(x,y,w,h)
 
         JButton b=new JButton("click me");
         b.setBounds(120,200,150,20);
 
+        // frame is not visible by default and layout is border by default
         f.setVisible(true);
         f.setLayout(null);
 
+        // adding objects to frame
         f.add(t1);
         f.add(b);
     }
--- GUI/MouseEvents/Test.java
+++ GUI/MouseEvents/Test.java
@@ -1,58 +1,56 @@
-import java.awt.*;
+// creating a frame with 2 textfield where t1 shows if the pointer is inside or outside the frame and t2 should display X and Y co-ordinates when user moves the pointer inside 
+
 import javax.swing.*;
 import java.awt.event.*;
-
-import org.w3c.dom.events.MouseEvent;
 
 class Example extends JFrame implements MouseListener,MouseMotionListener{
     JTextField t1,t2;
 
     Example(){
-        setSize(400,500);
-        setVisible(true);
-        setLayout(null);
+       setSize(400,500); 
+       setVisible(true);
+       setLayout(null);
 
         t1=new JTextField();
         t1.setBounds(100,100,200,20);
 
         t2=new JTextField();
-        t2.setBounds(100,200,200,30);
+        t2.setBounds(100,200,200,20);
 
         add(t1);
         add(t2);
 
+        // adding mouselistener and mousemotionlistener to frame
         addMouseListener(this);
         addMouseMotionListener(this);
     }
 
+    // function when mouse enters the frame
     public void mouseEntered(MouseEvent e){
         t1.setText("in");
     }
 
-    public void mouseExicted(MouseEvent e){
+    // function when mouse exits the frame
+    public void mouseExited(MouseEvent e){
         t1.setText("out");
     }
 
+    // function to display X and Y co-ordinates of the mouse
     public void mouseMoved(MouseEvent e){
-        String msg="x: "+((Component) e).getX()+"y: "+((Component) e).getY();
-        t2.setText(msg);
+        String info="X: "+e.getX()+" Y: "+e.getY();
+        t2.setText(info);
     }
 
-    @Override
-    public synchronized void addMouseListener(MouseListener l) {
-        // TODO Auto-generated method stub
-        super.addMouseListener(l);
-    }
-
-    @Override
-    public synchronized void addMouseMotionListener(MouseMotionListener l) {
-        // TODO Auto-generated method stub
-        super.addMouseMotionListener(l);
-    }
+    // all the functions must be overwrited when using mouse events
+    public void mouseDragged(MouseEvent e){}
+    public void mouseClicked(MouseEvent e){}
+    public void mousePressed(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){}
+    
 }
 
 class Test{
-    public static void main(String[] args) {
+    public static void main(String[] args){
         new Example();
     }
 }
