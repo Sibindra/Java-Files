68a51da607b8530b293482500d5bfb40554e0742
Sibindra Timalsina <timalsinasibindra@gmail.com>
2022-04-12 17:22:11 +0545

    adding another panel in layout

--- javaPanel/src/Test.java
+++ javaPanel/src/Test.java
@@ -1,18 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Test extends JPanel {
+public class Test {
     Test(){
         JFrame f=new JFrame("test frame");
         f.setSize(500,500);
 
-        JPanel p=new JPanel();
-        p.setBounds(100,100,100,100);
+        JPanel p1=new JPanel();
+        p1.setBackground(Color.RED);
 
-        f.setLayout(null);
-        p.setBackground(Color.RED);
+        JPanel p2=new JPanel();
+        p2.setBackground(Color.BLUE);
+
+        f.setLayout(new FlowLayout(FlowLayout.CENTER));
         f.setVisible(true);
-        f.add(p);
+        f.add(p1);
+        f.add(p2);
     }
 
     public static void main(String[] args) {
